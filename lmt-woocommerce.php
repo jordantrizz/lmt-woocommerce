c48432b1941041bb45f8be9cbbbcd3f031b24dc8
Jordan Trask <j@lmt.ca>
2020-03-10 15:10:23 +0000

    Updated plugin to allow for minimum quanities of 0 when dealing with grouped items

--- lmt-woocommerce.php
+++ lmt-woocommerce.php
@@ -13,19 +13,17 @@
  * Adjust the quantity input values
  */
 add_filter( 'woocommerce_quantity_input_args', 'jk_woocommerce_quantity_input_args', 10, 2 ); // Simple products
-
 function jk_woocommerce_quantity_input_args( $args, $product ) {
         if ( is_singular( 'product' ) ) {
                 $args['input_value']    = 1;    // Starting value (we only want to affect product pages, not cart)
         }
         $args['max_value']      = 80;   // Maximum value
-        $args['min_value']      = 1;    // Minimum value
+        $args['min_value']      = 0;    // Minimum value
         $args['step']           = 1;    // Quantity steps
         return $args;
 }
 
 add_filter( 'woocommerce_available_variation', 'jk_woocommerce_available_variation' ); // Variations
-
 function jk_woocommerce_available_variation( $args ) {
         $args['max_qty'] = 80;          // Maximum value (variations)
         $args['min_qty'] = 1;           // Minimum value (variations)
